6776756f465f2be755ab3b78502ba76eb588edd8
TheoObbard <theo@dali.dartmouth.edu>
2018-12-06 11:39:17 -0800

    Create selectAllPokemon selector

--- pokedex/frontend/pokedex.jsx
+++ pokedex/frontend/pokedex.jsx
@@ -6,11 +6,13 @@ import {
   requestAllPokemon
 } from "./actions/pokemon_actions";
 import { configureStore } from "./store/store";
+import { selectAllPokemon } from "./reducers/selectors";
 
 document.addEventListener("DOMContentLoaded", () => {
   var store;
   store = configureStore();
   window.requestAllPokemon = requestAllPokemon;
+  window.selectAllPokemon = selectAllPokemon;
   window.getState = store.getState;
   window.dispatch = store.dispatch;
   const rootEl = document.getElementById("root");
